eed08597780634f05b7dd4bb99dd97996d941e1a
BITJONG15 <nganbitjong@gmail.com>
2026-02-25 02:18:09 +0100

    Set Render and Netlify production URLs

--- public/js/config.js
+++ public/js/config.js
@@ -2,4 +2,5 @@ window.SECURECHAT_CONFIG = window.SECURECHAT_CONFIG || {};
 
 // Netlify deployment: set this to your Render backend URL.
 // Example: "https://securechat-backend.onrender.com"
-window.SECURECHAT_CONFIG.SOCKET_URL = window.SECURECHAT_CONFIG.SOCKET_URL || "";
+window.SECURECHAT_CONFIG.SOCKET_URL =
+  window.SECURECHAT_CONFIG.SOCKET_URL || "https://secureapp-0vkd.onrender.com";
